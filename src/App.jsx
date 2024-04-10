7cf98ad52bd9af0168518770e01044e296d76479
Preeti-Chavda <chavdapriti2001@gmail.com>
2024-04-11 01:20:34 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Create from './Create'
+import Home from './Home'
+import Update from './Update'
+import Read from './Read'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/create' element={<Create />}></Route>
+        <Route path='/update/:id' element={<Update />}></Route>
+        <Route path='/read/:id' element={<Read />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
